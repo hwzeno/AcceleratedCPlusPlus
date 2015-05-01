9784a7d1055a6051dac252e0a0b14249258952d1
zeno <quhwcn@gmail.com>
2015-05-01 00:00:00 -0400

    update

---
+++ chapter2/greetingX.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+int main()
+{
+	std::cout << "Please enter your name:";
+	std::string name;
+	std::cin >> name;
+
+	/*构造*/
+	const std::string greeting = "Hello," + name + "!";
+	
+	/**/
+	const int pad = 1;
+
+	const int rows = pad * 2 +3;
+	const std::string::size_type cols = greeting.size() + pad * 2 + 2;
+
+	std::cout << std::endl;
+
+	for(int r = 1; r != rows; ++r)
+	{
+		std::string::size_type c = 0;
+		
+
+		while(c != cols)
+		{
+			if(r == pad + 1 && c == pad + 1)
+			{
+				std::cout << greeting;
+				c += greeting.size();
+			}
+			else
+			{
+				if(r == 0 || r == rows - 1 || c == 0 || c == cols -1)
+					std::cout << "*";
+				else
+					std::cout << " ";
+				++c;
+			}
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
