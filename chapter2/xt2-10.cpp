8b31139f2764eb55a3b00cdb35032770d582251f
zeno <quhwcn@gmail.com>
2015-05-05 00:00:00 -0400

    update

---
+++ chapter2/xt2-10.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int k = 0;
+	while(k != 10)
+	{
+		using std::cout;
+		cout << "*";
+		k++;
+	}
+	std::cout << std::endl;
+	return 0;
+}
---
+++ chapter2/xt2-9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 0, b = 0;
+	cout << "input two number:" <<endl;
+	cin >> a >> b;
+
+	if(a > b)
+	{
+		cout << "a is larger." << endl;
+	
+	}
+	else
+	{
+		cout << "b is larger." << endl;
+	}
+	return 0;
+}
